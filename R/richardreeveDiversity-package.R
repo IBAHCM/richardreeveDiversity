7c244d030c8e999104d4af32bf6da8fa4dea3dfb
Richard Reeve <git@richardreeve.net>
2021-02-15 15:36:41 +0000

    Initial commit of package with basic documentation.

---
+++ R/richardreeveDiversity-package.R
@@ -0,0 +1,15 @@
+#' richardreeveDiversity R package
+#'
+#' A package that calculates the diversity of a bunch of communities derived
+#' from the Barro Colorado Island forest census plot.
+#'
+#'
+#' @author Richard Reeve <richard.reeve@glasgow.ac.uk>
+#'
+#' @name richardreeveDiversity-package
+#' @aliases richardreeveDiversity
+#' @docType package
+#'
+#' @import soniamitchellBCI
+#'
+NULL
